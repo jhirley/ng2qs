5eb5e2904976e41d143d07c568bf7d6c9d66433b
Jhirley <jhirley.fonte@gmail.com>
2016-08-20 21:09:01 -0400

    handling customers as Promise, then Observable, then just data

--- app/customer/customers.component.ts
+++ app/customer/customers.component.ts
@@ -11,13 +11,30 @@ import { Observable } from 'rxjs/Rx';
     providers: [CustomerService]
 })
 export class CustomersComponent implements OnInit {
-
-    customers: Observable<any[]>;
+    customers: any[];
+    // customers: Promise<any[]>;
+    // customers: Observable<any[]>;
+    
 
     constructor(private _customerService: CustomerService) { }
 
     ngOnInit() { 
-        this.customers = this._customerService.getCustomers();
+        this._customerService.getCustomers()
+        .then((customers) => this.customers = customers)
+        .catch((err) => {
+            console.log(err);
+        });
+        //////////////////////////
+        // Promise version
+        //  this.customers = this._customerService.getCustomers().catch((err) => {
+        //     console.log(err);
+        //     return Observable.of(true);
+        // });
+        ////////////////////////////////
+        // Rx Observable version
+        // this.customers = this._customerService.getCustomers().catch((err) => {
+        //     console.log(err);
+        //     return Observable.of(true);
+        // });
     }
-
 }
